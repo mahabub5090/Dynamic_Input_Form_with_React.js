149a4a0b886d4caf9a36bd3449974fff1acae45c
mahabub5090 <mdmahabub.rahman789@gmail.com>
2025-07-16 16:43:49 +0600

    Null handaling

--- src/Components/FormState.jsx
+++ src/Components/FormState.jsx
@@ -1,5 +1,6 @@
 const FormState=()=>{
     const showData=JSON.parse(localStorage.getItem("showData"));
+    if(showData==null)return;
     return (<>
     {
         <section className="grid mx-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
