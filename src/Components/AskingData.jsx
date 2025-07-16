ce0b248b871b3917763a8f95a5887746cc4a734c
mahabub5090 <mdmahabub.rahman789@gmail.com>
2025-07-16 19:40:20 +0600

    Every thing is ready. No just need to validate in Submit button

--- src/Components/AskingData.jsx
+++ src/Components/AskingData.jsx
@@ -1,27 +1,51 @@
-  import {  useState } from "react";
+  import {  useEffect, useRef, useState } from "react";
   import { toast, ToastContainer } from "react-toastify";
 
   function AskingData({setRender,addInputField}){
-      const [name,setName]=useState('');
-      const [options,setoptions]=useState('');
       const [clicked,setClicked]=useState(0);
 
       const handleAddClick=()=>{
+        const name=localStorage.getItem("fieldName");
+        const options=localStorage.getItem("fieldOption");
+
         if(name.trim().length==0 || options.trim().length==0){
           alert("Please give the field name and it's options. Then try again.")
           return;
         }        
         addInputField({name,options});
+        localStorage.removeItem("fieldName");
+        localStorage.removeItem("fieldOption");
         toast.success("Your input field was added successfully! ☺️");
         setTimeout(()=>{setRender(0)},3000);
         setClicked(1);
       }
       
+      const updateFieldName=(value)=>{
+        localStorage.removeItem("fieldName");
+        localStorage.setItem("fieldName",value);
+      }
+      const updateFieldOption=(option)=>{
+        localStorage.removeItem("fieldOption");
+        localStorage.setItem("fieldOption",option);
+      }
+      
+      const refer=useRef();
+      const refer2=useRef();
+
+      useEffect(()=>{
+        const name=localStorage.getItem("fieldName");
+        const options=localStorage.getItem("fieldOption");
+        if(name || options){
+          refer.current.value=name;
+          refer2.current.value=options;
+        }
+      },[]);
+
       return ( 
       <section className="bg-gray-700 mx-10 my-3 rounded-2xl">
             {clicked==0?<div className="grid grid-cols-1 gap-5 p-3 ">
-            <input onChange={(e)=>setName(e.target.value)} type="text" name="" id="" className="bg-black text-white  text-2xl rounded-2xl p-3" placeholder="New Input Field Name"/>
-            <input  onChange={(e)=>setoptions(e.target.value)} type="text" name="" id="" className="bg-black text-white text-2xl rounded-2xl p-3 " placeholder="Options [Seperate Using ,(Coma)]"/>
+            <input  ref={refer} onChange={(e)=>updateFieldName(e.target.value)} type="text" name="" id="" className="bg-black text-white  text-2xl rounded-2xl p-3" placeholder="New Input Field Name"/>
+            <input ref={refer2} onChange={(e)=>updateFieldOption(e.target.value)} type="text" name="" id="" className="bg-black text-white text-2xl rounded-2xl p-3 " placeholder="Options [Seperate Using ,(Coma)]"/>
             <div className="grid grid-cols-2 gap-2">
               <button onClick={()=>setRender(0)} className="bg-gray-500 text-black text-2xl rounded-2xl p-3">Cancel</button>
              <button onClick={handleAddClick} className="bg-white text-black text-2xl rounded-2xl p-3">+ Add</button>
